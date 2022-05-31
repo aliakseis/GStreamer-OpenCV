ce53316397ba2258b0ffc5e7066497814ee57740
aliakseis <aliaksei.sanko@gmail.com>
2022-05-31 14:22:11 +0300

    initial revision

--- GStreamer-OpenCV.cpp
+++ GStreamer-OpenCV.cpp
@@ -1,20 +1,41 @@
-// GStreamer-OpenCV.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// GStreamer-OpenCV.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+
+#include <opencv2/opencv.hpp>
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // The sink caps for the 'rtpjpegdepay' need to match the src caps of the 'rtpjpegpay' of the sender pipeline
+    // Added 'videoconvert' at the end to convert the images into proper format for appsink, without
+    // 'videoconvert' the receiver will not read the frames, even though 'videoconvert' is not present
+    // in the original working pipeline
+    //VideoCapture cap("udpsrc port=5000 ! application/x-rtp,media=video,payload=26,clock-rate=90000,encoding-name=JPEG,framerate=30/1 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink",
+    //    CAP_GSTREAMER);
+
+    // ksvideosrc ! video/x-raw,format=I420,width=640,height=480,framerate=30/1
+    //VideoCapture cap("ksvideosrc ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 ! videoconvert ! appsink",
+    cv::VideoCapture cap("ksvideosrc ! video/x-raw ! videoconvert ! appsink",
+        cv::CAP_GSTREAMER);
+
+    if (!cap.isOpened()) {
+        std::cerr << "VideoCapture not opened" << std::endl;
+        exit(-1);
+    }
+
+    while (true) {
+
+        cv::Mat frame;
+
+        cap.read(frame);
+
+        cv::imshow("receiver", frame);
+
+        if (cv::waitKey(1) == 27) {
+            break;
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
